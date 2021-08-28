9d35aad68a8b88dc31651a373c82e2a58fa12349
musstaffali <mamustafa@gtcc.edu>
2021-08-28 12:23:11 -0400

    engine input

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="7dc8d568-35e0-4f46-95b2-b7ddedb481d2"
+            userName="mamusstaffali"
+            userSecret="cat"
+        />
+    )
+}
+
+export default App;
